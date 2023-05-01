df6a01223ec44ad7a53e756d03eb92840f2cc010
Bhushan <nikam.soft@gmail.com>
2023-05-01 11:39:39 +0530

    hi

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose")
 const _ = require("lodash");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
@@ -131,6 +132,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
